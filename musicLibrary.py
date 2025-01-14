a57674f9955c7a3ef018166419f974e25432ae41
Bhagyashri1811 <87220665+Bhagyashri1811@users.noreply.github.com>
2025-01-13 16:12:19 -0800

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,8 @@
+music ={
+    "black":"https://www.youtube.com/watch?v=AMP5pIcrq4I",
+    "sadness" : "https://www.youtube.com/watch?v=wEWF2xh5E8s",
+    "rain":"https://www.youtube.com/watch?v=_6N9f1ad9Ow",
+    "buddha":"https://www.youtube.com/watch?v=h4RRkuqalpM"
+
+
+}
